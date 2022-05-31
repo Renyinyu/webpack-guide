00fbabe810a01cef0eb981929134ef3d9a95a995
chenjian <471630805@qq.com>
2022-06-01 01:20:49 +0800

    chore: 管理资源

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import _ from "lodash";
+import "./assets/styles/style.css";
+import "./assets/styles/style.scss";
+import Knife from "./assets/imgs/knife.jpg";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
 
+  const random = Math.random();
+
+  element.classList.add(random > 0.5 ? "hello" : "hello-sass");
   return element;
 }
 
+function imageComponent(isBg = false) {
+  if (isBg) {
+    const element = document.createElement("div");
+    element.classList.add("libai");
+    return element;
+  }
+  const image = new Image();
+  image.src = Knife;
+  return image;
+}
+
 document.body.appendChild(component());
+document.body.appendChild(component());
+document.body.appendChild(imageComponent());
+document.body.appendChild(imageComponent(true));
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,33 @@ module.exports = {
   entry: "./src/index.js",
 
   output: {
-    filename: "[name].js",
+    filename: "bundle.js",
     path: resolve("dist")
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"]
+      },
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024 // 8kb
+          }
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource"
+      }
+    ]
   }
 };
